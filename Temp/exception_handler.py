dfa82f29092ce5a4bf95051f85051583aacdb04e
javadhoseinzade <javadhoseinzde80@gmail.com>
2025-10-21 20:45:47 +0330

    fix exception_handler with import Response

--- Temp/exception_handler.py
+++ Temp/exception_handler.py
@@ -1,6 +1,7 @@
 from rest_framework.views import exception_handler
 from rest_framework import status
 from .message import result_message
+from rest_framework.response import Response
 
 def custom_exception_handler(exc, context):
     response = exception_handler(exc, context)
