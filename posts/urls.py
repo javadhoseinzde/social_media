3dd5815ce2f8ce8504bdf5f37de5a1bbce5b0282
javadhoseinzade <javadhoseinzde80@gmail.com>
2025-10-21 20:48:57 +0330

    refactor(urls): clean and standardize post endpoints

--- posts/urls.py
+++ posts/urls.py
@@ -3,7 +3,6 @@ from django.urls import path
 from .views.post_views import PostApi, PostDetailApi
 
 urlpatterns = [
-    path("post-list/", PostApi.as_view(), name="post-list"),
-    path("create-post/", PostApi.as_view(), name="create-post"),
+    path("posts", PostApi.as_view(), name="posts"),
     path("post-detail/<int:id>/", PostDetailApi.as_view(), name="post-detail")
 ]
